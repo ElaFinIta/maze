a11ad1bf04ea7838a2b0672d7ba890fcfae2627e
Ele Na <elena.china-kolehmainen@edu.bc.fi>
2021-12-15 17:42:49 +0200

    favicon and social media/contact icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import '../App.css';
+import insta from './assets/instagram.svg';
+import github from './assets/github.svg';
+import stackoverflow from './assets/stack-overflow.svg';
+import linkedin from './assets/linkedin.svg';
 
 const Footer = () => {
     return (
         <div className="footer">
-            ECK 2021
+            <a href="https://stackoverflow.com/users/14586072/elanor" target="_blank" rel="noreferrer">
+            <img src={stackoverflow} alt="stack-overflow icon" />
+            </a>
+            
+            <a href="https://github.com/ElaFinIta" target="_blank" rel="noreferrer">
+            <img src={github} alt="github icon" />
+            </a>
+
+            <a href="https://www.linkedin.com/in/elena-china-kolehmainen-958673143/" target="_blank" rel="noreferrer">
+            <img src={linkedin} alt="linkedin icon" />
+            </a>
+
+            <a href="https://www.instagram.com/leggi_con_me/" target="_blank" rel="noreferrer">
+            <img src={insta} alt="instagram icon" />
+            </a>
+            <p>ECK 2021</p>
         </div>
     );
 };
